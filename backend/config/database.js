e96d939ff0b365d064bff8d9dd75b5ba652b5c93
Gayatri Sudhir Deshpande <gayatrida05@gmail.com>
2026-02-28 18:36:24 +0530

    Update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -15,8 +15,9 @@ const pool = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
   ssl: {
-    minVersion: 'TLSv1.2',
-    rejectUnauthorized: true
+    ca: fs.readFileSync(
+      path.join(__dirname, 'DigiCertGlobalRootCA.crt.pem')
+    )
   }
 });
 // Test connection
